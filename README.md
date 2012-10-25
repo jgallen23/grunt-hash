6f866754a654bbcdc6ba1014261aec35fbd6d569
Greg Allen <me@jga.me>
2012-10-24 20:09:56 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# grunt-hash
+
+Append a unique hash to tne end of a file for cache busting.
+
+## Getting Started
+Install this grunt plugin next to your project's [grunt.js gruntfile][getting_started] with: `npm install grunt-hash`
+
+Then add this line to your project's `grunt.js` gruntfile:
+
+```javascript
+grunt.loadNpmTasks('grunt-hash');
+```
+
+[grunt]: http://gruntjs.com/
+[getting_started]: https://github.com/gruntjs/grunt/blob/master/docs/getting_started.md
+
+## Documentation
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt][grunt].
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Greg Allen  
+Licensed under the MIT license.
---
+++ tasks/hash.js
@@ -0,0 +1,56 @@
+/*
+ * grunt-hash
+ * https://github.com/jgallen23/grunt-hash
+ *
+ * Copyright (c) 2012 Greg Allen
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+
+  // Please see the grunt documentation for more information regarding task and
+  // helper creation: https://github.com/gruntjs/grunt/blob/master/docs/toc.md
+
+  // ==========================================================================
+  // TASKS
+  // ==========================================================================
+
+  grunt.registerTask('hash', 'Append a unique hash to tne end of a file for cache busting.', function() {
+    grunt.config.requires(['hash', 'src']);
+    var fs = require('fs');
+    var path = require('path');
+    fs.existsSync = fs.existsSync || path.existsSync;
+    var getHash = require('../lib/hash');
+
+    var options = grunt.config('hash');
+    options.dest = options.dest || '';
+    var map = {};
+
+    grunt.file.expandFiles(options.src).forEach(function(file) {
+
+      var source = fs.readFileSync(file, 'utf8'); 
+      var hash = getHash(source);
+      var ext = path.extname(file);
+      var basename = path.basename(file, ext);
+
+      var newFile = basename+'.'+hash+ext;
+      var newPath = path.join(options.dest, newFile);
+
+      if (!fs.existsSync(newPath)) {
+        fs.writeFileSync(newPath, source);
+        grunt.log.writeln('Generated: '+newPath);
+      } else {
+        grunt.log.writeln('Skipping: '+newPath);
+      }
+      map[basename+ext] = newFile;
+    });
+    if (options.mapping) {
+      fs.writeFileSync(options.mapping, JSON.stringify(map));
+      grunt.log.writeln('Generated mapping: '+options.mapping);
+    }
+
+  });
+
+
+
+};
